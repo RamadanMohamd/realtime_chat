627e7f677486283e37366d5c57e2ea7fe9fa0f9a
Ramadan <ramadan@Ramadans-MBP.home>
2023-05-01 15:44:38 +0300

    auth with next-auth

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Button from '@/components/ui/Button'
-import { db } from '@/lib/db'
+import Button from '../components/ui/Button'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default async function Home() {
-  await db.set("hello", 'hello')
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Button isLoading={false}>Submit</Button>
